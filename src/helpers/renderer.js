5609593ebc31ce41de1fb9b46b7a52b577f6182e
Eirini Pappa <eirini.pappa@optis.be>
2017-10-31 14:21:12 +0100

    add Routes

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -5,7 +5,7 @@ import Home from '../client/components/Home';
 export default () => {
   const content = renderToString(<Home />);
 
-  return `
+  return`
     <html>
       <head></head>
       <body>
