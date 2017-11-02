d1d44a3b6656d9dc974f39127d6f4d388850da40
eyerean <eptinea@gmail.com>
2017-11-02 15:36:09 +0100

    add auth actionCreator + reducer

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,5 +1,4 @@
 export const FETCH_USERS = 'fetch_users';
-
 export const fetchUsers = () => async (dispatch, getState, api) => {
   const res = await api.get('/users');
 
@@ -8,3 +7,14 @@ export const fetchUsers = () => async (dispatch, getState, api) => {
     payload: res
   });
 };
+
+export const FETCH_CURRENT_USER = 'fetch_current_user';
+export const fetchCurrentUser = () => async (dispatch, getState, api) => {
+  const res = await api.get('/current_user');
+
+  dispatch({
+    type: FETCH_CURRENT_USER,
+    payload: res
+  });
+};
+
