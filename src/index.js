3beb46c7da2f87ff7ff9a82e66558efffa46cf93
eyerean <eptinea@gmail.com>
2017-11-02 15:11:30 +0100

    fixed typo

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import createStore from './helpers/createStore';
 
 const app = express();
 
-app.use('/api', proxy('http://react-ssr=api.herokuapp.com', {
+app.use('/api', proxy('http://react-ssr-api.herokuapp.com', {
   proxyReqOptDecorator(opts){
     opts.header['x-forwareded-host'] = 'localhost:3000';
     return opts;
