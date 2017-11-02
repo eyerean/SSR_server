078012f9c48a71e227fb1687352e366d4e766adf
eyerean <eptinea@gmail.com>
2017-11-02 15:59:16 +0100

    connect the header, add login-logout logic

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
-export default () => {
+const Header = ({auth}) => {
+  const authButton = auth ? (
+    <a href="/api/logout">Logout</a>
+  ) : (
+    <a href="/api/auth/google">Login</a>
+  );
+  console.log('auth', auth);
+
   return(
     <div>
       <Link to="/">React SSR</Link>
+      <div>        
+        <Link to="/users">Users</Link>
+        <Link to="/admins">Admins</Link>
+        {authButton}
+      </div>
     </div>
   );
+};
+
+const mapStateToProps = ({auth}) => {
+  return {auth};
 }
+
+export default connect(mapStateToProps)(Header);
